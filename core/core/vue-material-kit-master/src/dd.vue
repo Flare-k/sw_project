1df23eec7b7ec36a7b4c2d1a728270537a5761e7
rkdwlss2 <61217289+rkdwlss2@users.noreply.github.com>
2021-08-30 22:46:17 +0900

    jsfile

---
+++ core/core/vue-material-kit-master/src/dd.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="wrapper">
+    <parallax
+      class="section page-header header-filter"
+      :style="headerStyle"
+    ></parallax>
+    <div class="main main-raised">
+      <div class="player">
+        <div class="container1">
+          <div class="md-layout1">
+            <div class="md-layout-item1">
+              <div class="profile" id="test">
+                <div class="avatar1">
+                  <img
+                    :src="img"
+                    alt="Circle Image"
+                    class="img-raised rounded-circle img-fluid"
+                  />
+                </div>
+                <div class="name">
+                  <h3 class="title">Ralo</h3>
+                </div>
+              </div>
+            </div>
+          </div>
+      </div>
+           <div class="player">
+            <iframe width="1000" height="1000" src="https://www.youtube.com/embed/CNvN9wPx2Ag" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          </div>
+    </div>
+        </div>
+             
+
+  </div>
+</template>
+
+<script>
+import { Tabs } from "@/components";
+export default {
+  components: {
+   
+  },
+  bodyClass: "profile-page",
+
+  props: {
+    header: {
+      type: String,
+      default: require("@/assets/img/city-profile.jpg")
+    },
+    img: {
+      type: String,
+      default: require("@/assets/img/faces/raloPic.webp")
+    }
+  },
+  computed: {
+    headerStyle() {
+      return {
+        backgroundImage: `url(${this.header})`
+      };
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+#test{
+  max-height: 1000px;
+}
+.player{
+  max-height: 500px;
+}
+
+</style>
---
+++ core/core/vue-material-kit-master/src/views/Profile.vue
@@ -0,0 +1,170 @@
+<template>
+  <div class="wrapper">
+    <parallax
+      class="section page-header header-filter"
+      :style="headerStyle"
+    ></parallax>
+    <div class="main main-raised">
+      <div class="section profile-content">
+        <div class="container">
+          <div class="md-layout">
+            <div class="md-layout-item md-size-50 mx-auto">
+              <div class="profile">
+                <div class="avatar">
+                  <img
+                    :src="img"
+                    alt="Circle Image"
+                    class="img-raised rounded-circle img-fluid"
+                  />
+                </div>
+                <div class="name">
+                  <h3 class="title">{{videoId.name}}</h3>
+
+                </div>
+              </div>
+            </div>
+          </div>
+          <div id="app">
+                      <iframe class="ifa" width="2500" height="800" v-bind:src="Link" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          </div>
+          <div class="profile-tabs">
+            <tabs
+              :tab-name="['Twitch', 'Afreeca', 'Youtube']"
+              :tab-icon="['camera', 'palette', 'favorite']"
+              plain
+              nav-pills-icons
+              color-button="success"
+            >
+              <!-- here you can add your content for tab-content -->
+              <template slot="tab-pane-1">
+                <div class="md-layout">
+                  <div class="md-layout-item md-size-25 ml-auto">
+                    <img :src="tabPane1[0].image" class="rounded" />
+                    <img :src="tabPane1[1].image" class="rounded" />
+                  </div>
+                  <div class="md-layout-item md-size-25 mr-auto">
+                    <img :src="tabPane1[3].image" class="rounded" />
+                    <img :src="tabPane1[2].image" class="rounded" />
+                  </div>
+                </div>
+              </template>
+              <template slot="tab-pane-2">
+                <div class="md-layout">
+                  <div class="md-layout-item md-size-25 ml-auto">
+                    <img :src="tabPane2[0].image" class="rounded" />
+                    <img :src="tabPane2[1].image" class="rounded" />
+                    <img :src="tabPane2[2].image" class="rounded" />
+                  </div>
+                  <div class="md-layout-item md-size-25 mr-auto">
+                    <img :src="tabPane2[3].image" class="rounded" />
+                    <img :src="tabPane2[4].image" class="rounded" />
+                  </div>
+                </div>
+              </template>
+              <template slot="tab-pane-3">
+                <div class="md-layout">
+                  <div class="md-layout-item md-size-25 ml-auto">
+                    <img :src="tabPane3[0].image" class="rounded" />
+                    <img :src="tabPane3[1].image" class="rounded" />
+                  </div>
+                  <div class="md-layout-item md-size-25 mr-auto">
+                    <img :src="tabPane3[2].image" class="rounded" />
+                    <img :src="tabPane3[3].image" class="rounded" />
+                    <img :src="tabPane3[4].image" class="rounded" />
+                  </div>
+                </div>
+              </template>
+            </tabs>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { Tabs } from "@/components";
+
+
+
+export default {
+  
+  components: {
+    Tabs
+  },
+  bodyClass: "profile-page",
+  data() {
+    return {
+      tabPane1: [
+        { image: require("@/assets/img/twitch1.png") },
+        { image: require("@/assets/img/twitch2.png") },
+        { image: require("@/assets/img/twitch3.png") },
+        { image: require("@/assets/img/twitch4.png") }
+      ],
+      tabPane2: [
+        { image: require("@/assets/img/afreeca1.png") },
+        { image: require("@/assets/img/afreeca2.png") },
+        { image: require("@/assets/img/afreeca3.png") },
+        { image: require("@/assets/img/afreeca1.png") },
+        { image: require("@/assets/img/afreeca2.png") }
+      ],
+      tabPane3: [
+        { image: require("@/assets/img/you1.png") },
+        { image: require("@/assets/img/you2.png") },
+        { image: require("@/assets/img/you3.png") },
+        { image: require("@/assets/img/you1.png") },
+        { image: require("@/assets/img/you1.png") }
+      ]
+      
+    };
+  },
+  props: {
+    header: {
+      type: String,
+      default: require("@/assets/img/city-profile.jpg")
+    },
+    img: {
+      type: String,
+      default: require("@/assets/img/faces/raloPic.webp")
+    }
+  },
+  computed: {
+    videoId(){
+      return this.$route.params
+    },
+
+    headerStyle() {
+      return {
+        backgroundImage: `url(${this.header})`
+      };
+    },
+    Link(){
+      return "https://www.youtube.com/embed/"+this.$route.params.videoId
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.ifa{
+  min-height:750px;
+}
+.section {
+  padding: 0;
+}
+
+.profile-tabs::v-deep {
+  .md-card-tabs .md-list {
+    justify-content: center;
+  }
+
+  [class*="tab-pane-"] {
+    margin-top: 3.213rem;
+    padding-bottom: 50px;
+
+    img {
+      margin-bottom: 2.142rem;
+    }
+  }
+}
+</style>
