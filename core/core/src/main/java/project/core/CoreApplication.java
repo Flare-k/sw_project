8b241d1c039fd45e6218005b9aa4064ee409658c
rkdwlss2 <61217289+rkdwlss2@users.noreply.github.com>
2021-08-30 22:43:05 +0900

    vue.js

--- core/core/src/main/java/project/core/CoreApplication.java
+++ core/core/src/main/java/project/core/CoreApplication.java
@@ -1,13 +1,20 @@
 package project.core;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import project.core.controller.CrollingController;
+
+import java.io.IOException;
 
 @SpringBootApplication
 public class CoreApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		SpringApplication.run(CoreApplication.class, args);
+
+//		CrollingController crollingController=new CrollingController();
+//		crollingController.getDataFromWeb();
 	}
 
 }
